9bcb1874f5cbfb255f1c602a2b8d4c09c1f5fef8
David Mendelovits <david.mendelovits@gmail.com>
2017-12-11 21:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,2 @@
 console.error("Help Me")
-console.log9("Logging harder than Dexter at the end of season 8")
-console.warn("Woah there! Back up Fella.")
+console.log("Logging harder than Dexter at the end of season 8")
